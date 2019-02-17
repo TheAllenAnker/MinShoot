34c9906cf1eb4e28669063b3a60f16f44b306c9c
TheAllenAnker <theallenanker@gmail.com>
2019-02-17 20:33:02 +0800

    Add list demo.

---
+++ pages/list/list.js
@@ -0,0 +1,26 @@
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    usernames: ['Allen Anker', 'Taylor Swift', 'Troye Sivan'],
+    nicknames_dict: [
+      {name: 'Allen Anker', age: '19'},
+      {name: 'Taylor Swift', age: '29'}
+    ]
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
